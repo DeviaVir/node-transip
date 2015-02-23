061d9c5bbaa7c3aca78dced648bd145b0665241e
Chase Sillevis <chase@sillevis.net>
2015-02-23 13:28:12 +0100

    Use locked transip account for testing

---
+++ config/data.js
@@ -0,0 +1,36 @@
+var config = {
+  "transip": {
+    "login": "deviavir",
+    "privateKey": 
+      "-----BEGIN PRIVATE KEY-----\n" + 
+      "MIIEwAIBADANBgkqhkiG9w0BAQEFAASCBKowggSmAgEAAoIBAQDeAHTkYQJ6IDOC\n" + 
+      "6yqbQwy1um2Af5/cGxwxH1C1b2fVL4coFeQ4FK/GZqyEIggSMFbVKzFTcK5mP57e\n" + 
+      "5RK9QiM46BhRMXDXURY3OS2ZZqj4m2uNqYqmSU0O5WZQvbDBYLBxvtY6/3m8nfOC\n" + 
+      "OaQRtMDPzotNgbWnUksaS9LkLMPxd2Dw7enx5Q+uXhjYPsNX0+E66TFpsSHuZU1S\n" + 
+      "haIXxBy2L0TUwcnBPy5KNxgkE9E4lur1By55cX1U0De37FPNFKpcElXttubUuit/\n" + 
+      "Tc1JYt388F4R8D0R4G/KV6pMXou3hLE704cbgu9Ycpo6BSi4f2m9/S2fosjtrV0Y\n" + 
+      "rTkglPN3AgMBAAECggEBAMp4SiO74iF12uUNC7e86AuZ8qKeQzLgOs8GS3qeF2nU\n" + 
+      "RgUmKhWHsGRXYgHEc+36zkbudLcv7q7sWT3aWEPp3OvaqcrrN4n4tzU4XcEpYvQR\n" + 
+      "836o4P4VnkT2rWD4EAvd3i5vKT13oRSITf04Cs3nN26PZncS1JS8tihOfRGDTlWa\n" + 
+      "otQyMcO7+I0tilkpQHX6o1zP4vc+syK40XJf1N2B/M/vPIHAJ31Pi2OaEOoHlORB\n" + 
+      "7l65K9ozkcQv9jVpqpJb+n4vQFNJa/KvEWTVYLTZfyb6ius/d2sEIsTSmjELlb2G\n" + 
+      "1nv5Ph1QAcVGDrrLCA9s9A2TxvaPmUs9u9SSCFnyg9ECgYEA94YnpmzOVNtJz5jj\n" + 
+      "QCWXASbAs2fEJ9HZr1bpzyRZKgfiKHud5uFZdVeD99a1zyHfq23BSnBivcYzKHZQ\n" + 
+      "t+vtiq3UA/CZKw9HxscB0Z4nPKcO+DqPqr9WFR7JE9CjBQjYzOcaaTyFDw5WG3RP\n" + 
+      "zcnSzueigG+uEN897631HL1vWT8CgYEA5ZqRij7esru4I50qbkbqbzThG7HVs5O4\n" + 
+      "bfgtQuPmqm1AJxk/ZR557/rYE+tr0AnCrUTuXFJ88jEhHJ0HgJrsAYXzL3pqsr8b\n" + 
+      "oLiX43xrMW1GjNj9gtzq/zyOi4DRQk/k+uudZqMX3o/vCQhr8epHoXgs/df1gfZt\n" + 
+      "yWXG9MYW38kCgYEAmQT7kBwltjnWuJr4qhhS8OhHKuHUVxWZMJdXdbIv2RcEjihV\n" + 
+      "pnzbs/oFYHsA7GsxfiYc32r47/8Oc04penpE4H2z2zerSP5qTg2LJ05RTWKtk3GI\n" + 
+      "jXM5wVbF0hUiIqOke4/HORWr4HHpc5GFIGBLjZOPVz0za8TG4lw1cq39nzsCgYEA\n" + 
+      "saXlY9R/FEVgFNYepVJjQwXkAjyXPylVcmiqBG4247hCAb5sKX3qFL69/rORKFbU\n" + 
+      "9KE31+DO4ckXeH8a9etoHUmZvit2cLSi2LjadF2oxoZpJsMhauYpQLCHDfLudeNB\n" + 
+      "JM+2alpYNpCg/xkwA+4j3BASp5k3UIXtrfz8chjuS9kCgYEA1sRmfJ96FBNchBuI\n" + 
+      "c8ukLvP5NkJXPnsCbZDQAwzrRRQwzwNGfg4WvhFXNfm7AYVesKfWbFIeePchCJKY\n" + 
+      "ctZTsr0/lWnz95toFSuVYWKzef6jTnXY4QQ0GLxawQzczQQuvzx7hoj0W05CAIu2\n" + 
+      "vdKLyhpDBkavyB6kF5ajoonWBwc=\n" + 
+      "-----END PRIVATE KEY-----"
+  }
+};
+
+module.exports = config;
